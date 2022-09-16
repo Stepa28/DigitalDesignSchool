c7a4d14a5c6193ee8b3e4a77eb08a86daea266ef
Stepa <cntgf.2010@yandex.ru>
2022-09-16 16:56:00 +0300

    Task done

---
+++ DigitalDesignSchool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите путь до текстового файла");
+var filePath = Console.ReadLine(); //Получение путя до текстового файла
+
+try
+{
+    var allText = File.ReadAllText(filePath);                         //Чтение текстового файла
+    var enumerableWords = Regex.Matches(allText, @"\b\w+\b")          //Получение списка слов
+                               .Select(x => x.Value)                  //Выбор строк
+                               .GroupBy(x => x)                       //Группировка по отдельным словам
+                               .OrderByDescending(x => x.Count())     //Сортировка по убыванию
+                               .Select(x => $"{x.Key}\t{x.Count()}"); //Форма записи
+
+    File.WriteAllText(@"WordsInText.txt",      //Запись получившегося результата
+        string.Join("\r\n", enumerableWords)); //Склейка полученных результатов, каждый с новой строки
+
+
+    Console.WriteLine("Приложение закончило свою работу");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Во время работы приложения возникло исключение :\n{e}");
+    throw;
+}
