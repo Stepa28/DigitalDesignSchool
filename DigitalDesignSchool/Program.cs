fc8f3915e8f2152aa254780bcc95e7f0b09a3a95
Stepa <cntgf.2010@yandex.ru>
2022-10-12 21:49:33 +0300

    Add Parallel.ForEach and use ConcurrentDictionary

--- DigitalDesignSchool/Program.cs
+++ DigitalDesignSchool/Program.cs
@@ -1,15 +1,21 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Reflection;
 using PrivateLibrary;
 
 string filePath = "C:\\Project\\DigitalDesignSchool\\Text.txt";
 
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
 var couter = new WordsCounter();
 var method = couter.GetType().GetMethod("WordsCounterFromPathFile", BindingFlags.NonPublic | BindingFlags.Instance);
 
 object[] parameters = { filePath };
-var words = method.Invoke(couter, parameters) as Dictionary<string, int>;
+var words = method.Invoke(couter, parameters) as ConcurrentDictionary<string, int>;
 
 File.WriteAllText(@"WordsInText.txt", //Запись получившегося результата
     string.Join("\r\n",
         words.OrderByDescending(x => x.Value)
              .Select(word => $"{word.Key}\t{word.Value}"))); //Склейка полученных результатов, каждый с новой строки
+             
+Console.WriteLine(stopWatch.Elapsed);
--- PrivateLibrary/WordsCounter.cs
+++ PrivateLibrary/WordsCounter.cs
@@ -1,26 +1,20 @@
-﻿namespace PrivateLibrary;
+﻿using System.Collections.Concurrent;
+
+namespace PrivateLibrary;
 
 public class WordsCounter
 {
-    private Dictionary<string, int> WordsCounterFromPathFile(string path)
+    private ConcurrentDictionary<string, int> WordsCounterFromPathFile(string path)
     {
         try
         {
-            var words = new Dictionary<string, int>();
+            var words = new ConcurrentDictionary<string, int>();
             var allText = File.ReadAllText(path); //Чтение текстового файла
 
             char[] chars = { ' ', '.', ',', ';', ':', '?', '\n', '\r', '"', '(', ')', '[', ']', '!' }; // разделители слов
             string[] enumerableWords = allText.Split(chars, StringSplitOptions.RemoveEmptyEntries);    //Получение массива списка слов
 
-            foreach (string word in enumerableWords)
-            {
-                string w = word.ToLower(); //все слова в нижнем регистре
-
-                if (!words.ContainsKey(w))
-                    words.Add(w, 1); //если слова нет в словаре, то добавляем
-                else
-                    words[w] += 1; //если слово есть в словаре, добавляем счётчику 1
-            }
+            Parallel.ForEach(enumerableWords, word => words.AddOrUpdate(word.ToLower(), 1, (_, i) => ++i));
             return words;
         }
         catch (Exception e) //Т.к. задача учебная обработчик всех возможных ошибок не делал
