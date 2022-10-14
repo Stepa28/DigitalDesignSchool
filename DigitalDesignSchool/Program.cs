dc00a1e350c7fbecb812a9ecacfa304ecbf35df5
Stepa <cntgf.2010@yandex.ru>
2022-10-14 19:31:35 +0300

    Finalization of the third homework

--- DigitalDesignSchool/Program.cs
+++ DigitalDesignSchool/Program.cs
@@ -11,11 +11,21 @@ var couter = new WordsCounter();
 var method = couter.GetType().GetMethod("WordsCounterFromPathFile", BindingFlags.NonPublic | BindingFlags.Instance);
 
 object[] parameters = { filePath };
-var words = method.Invoke(couter, parameters) as ConcurrentDictionary<string, int>;
+var words = method.Invoke(couter, parameters) as Dictionary<string, int>;
 
 File.WriteAllText(@"WordsInText.txt", //Запись получившегося результата
     string.Join("\r\n",
         words.OrderByDescending(x => x.Value)
              .Select(word => $"{word.Key}\t{word.Value}"))); //Склейка полученных результатов, каждый с новой строки
              
+Console.WriteLine(stopWatch.Elapsed);
+
+stopWatch.Restart();
+
+var wordsParallel = couter.WordsCounterFromPathFileParallel(filePath);
+File.WriteAllText(@"WordsInText.txt", //Запись получившегося результата
+    string.Join("\r\n",
+        wordsParallel.OrderByDescending(x => x.Value)
+                     .Select(word => $"{word.Key}\t{word.Value}"))); //Склейка полученных результатов, каждый с новой строки
+
 Console.WriteLine(stopWatch.Elapsed);
--- PrivateLibrary/WordsCounter.cs
+++ PrivateLibrary/WordsCounter.cs
@@ -4,7 +4,7 @@ namespace PrivateLibrary;
 
 public class WordsCounter
 {
-    private ConcurrentDictionary<string, int> WordsCounterFromPathFile(string path)
+    public ConcurrentDictionary<string, int> WordsCounterFromPathFileParallel(string path)
     {
         try
         {
@@ -15,6 +15,34 @@ public class WordsCounter
             string[] enumerableWords = allText.Split(chars, StringSplitOptions.RemoveEmptyEntries);    //Получение массива списка слов
 
             Parallel.ForEach(enumerableWords, word => words.AddOrUpdate(word.ToLower(), 1, (_, i) => ++i));
+            return words;
+        }
+        catch (Exception e) //Т.к. задача учебная обработчик всех возможных ошибок не делал
+        {
+            Console.WriteLine($"Во время работы приложения возникло исключение :\n{e}");
+            throw;
+        }
+    }
+    
+    private Dictionary<string, int> WordsCounterFromPathFile(string path)
+    {
+        try
+        {
+            var words = new Dictionary<string, int>();
+            var allText = File.ReadAllText(path); //Чтение текстового файла
+
+            char[] chars = { ' ', '.', ',', ';', ':', '?', '\n', '\r', '"', '(', ')', '[', ']', '!' }; // разделители слов
+            string[] enumerableWords = allText.Split(chars, StringSplitOptions.RemoveEmptyEntries);    //Получение массива списка слов
+
+            foreach (string word in enumerableWords)
+            {
+                string w = word.ToLower(); //все слова в нижнем регистре
+
+                if (!words.ContainsKey(w))
+                    words.Add(w, 1); //если слова нет в словаре, то добавляем
+                else
+                    words[w] += 1; //если слово есть в словаре, добавляем счётчику 1
+            }
             return words;
         }
         catch (Exception e) //Т.к. задача учебная обработчик всех возможных ошибок не делал
